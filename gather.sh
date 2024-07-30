4b6ec4773089cdce2a8206be3b6f241ef69a1552
Brent Baude <bbaude@redhat.com>
2024-07-30 12:48:43 -0500

    Straightline the process after builds
    this PR streamlines what happens after the build is done including the
    manfiest creation and so forth.  the push still needs to be done.
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- gather.sh
+++ gather.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-arches=("x86_64" "aarch64")
-imagetypes=("applehv" "hyperv" "qemu")
+source ./util.sh
 
-if [ "$#" -ne 2 ]; then
-    echo "gather image-manifest-name base-coreos-name"
-    exit 1
-fi
-
-buildah manifest create $1
-
-for itype in "${imagetypes[@]}"
-do
-  for arch in "${arches[@]}"
-  do
-	#file=$(/bin/ls $2-$itype.$arch.*)
-	file=$(/bin/ls $2.$arch.$itype.*.zst)
-	if [[ -z "$file" ]]; then
-		echo "doh"
-		exit 1
-	fi
-	echo $file
-	# removals from first incantation --artifact-config-type="application/vnd.oci.image.config.v1+json" --artifact-layer-type=application/vnd.oci.image.layer.v1.tar
-	echo "buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${itype}" $1 $file"
-	buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${itype}" $1 $file
+buildah manifest create "${FULL_IMAGE_NAME}"
+# This section adds the OCI artifacts to the manifest
+for itype in "${DISK_FLAVORS[@]}"; do
+  for arch in "${ARCHES[@]}"; do
+    DISK_FORMAT=$(disk_format_from_flavor "${itype}")
+    COMPRESSED_DISK="${OUTDIR}/${DISK_IMAGE_NAME}.${arch}.${itype}.${DISK_FORMAT}.zst"
+	  echo "buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${itype}" ${FULL_IMAGE_NAME} ${COMPRESSED_DISK}"
+	  buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${itype}" "${FULL_IMAGE_NAME}" "$COMPRESSED_DISK"
   done
 done
+
+# This section adds the images to the manifest as well
+for arch in "${IMAGE_ARCHES[@]}"; do
+ buildah manifest add --arch $arch "${FULL_IMAGE_NAME}" "${FULL_IMAGE_NAME}-$arch"
+done
+
