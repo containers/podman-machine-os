e0f6d3fb85b8881206dfbcc56f6c563af2c0b908
Lokesh Mandvekar <lsm5@fedoraproject.org>
2024-10-18 16:40:56 +0530

    TMT: generate dev and release artifacts
    This patch allows building dev / release artifacts with the help of a
    few envvars in `podman-rpm-info.sh`. If `PODMAN_RPM_TYPE` is `dev`, it'll
    fetch podman and other relevant rpms from the `podman-next` copr. Else,
    it'll fetch the podman rpm from koji with version-release values as
    mentioned in `podman-rpm-info-vars.sh`.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- build.sh
+++ build.sh
@@ -4,26 +4,37 @@ set -exo pipefail
 
 source ./util.sh
 
-
 echo "Preparing to build ${FULL_IMAGE_NAME}"
 
-mkdir -p $OUTDIR
-git clone https://github.com/dustymabe/build-podman-machine-os-disks
+if [[ ! -d "build-podman-machine-os-disks" ]]; then
+    git clone https://github.com/dustymabe/build-podman-machine-os-disks
+fi
 
 echo " Building image locally"
 
-podman build -t "${FULL_IMAGE_NAME_ARCH}" -f podman-image-daily/Containerfile ${PWD}/podman-image-daily
+# See podman-rpm-info-vars.sh for all build-arg values. If PODMAN_RPM_TYPE is
+# "dev", the rpm version, release and fedora release values are of no concern
+# to the build process.
+podman build -t "${FULL_IMAGE_NAME_ARCH}" -f podman-image/Containerfile ${PWD}/podman-image \
+    --build-arg PODMAN_RPM_TYPE=${PODMAN_RPM_TYPE} \
+    --build-arg PODMAN_VERSION=${PODMAN_VERSION} \
+    --build-arg PODMAN_RPM_RELEASE=${PODMAN_RPM_RELEASE} \
+    --build-arg FEDORA_RELEASE=${FEDORA_RELEASE} \
+    --build-arg ARCH=${ARCH}
 
 echo "Saving image from image store to filesystem"
 
+mkdir -p $OUTDIR
 podman save --format oci-archive -o "${OUTDIR}/${DISK_IMAGE_NAME}" "${FULL_IMAGE_NAME_ARCH}"
 
 echo "Transforming OCI image into disk image"
 SRCDIR="${TMT_TREE:-..}"
-cd $OUTDIR && sudo sh $SRCDIR/build-podman-machine-os-disks/build-podman-machine-os-disks.sh "${PWD}/${DISK_IMAGE_NAME}"
+pushd $OUTDIR && sudo sh $SRCDIR/build-podman-machine-os-disks/build-podman-machine-os-disks.sh "${PWD}/${DISK_IMAGE_NAME}"
 
 echo "Compressing disk images with zstd"
 # note: we are still "in" the outdir at this point
 for DISK in "${DISK_FLAVORS_W_SUFFIX[@]}"; do
   zstd -T0 -14 "${DISK_IMAGE_NAME}.${CPU_ARCH}.${DISK}"
 done
+
+popd
--- util.sh
+++ util.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./podman-rpm-info-vars.sh
+
 CPU_ARCH=`lscpu --json | jq .lscpu.[0].data | tr -d '"'`
 ARCHES=("x86_64" "aarch64")
 IMAGE_ARCHES=("amd64" "arm64")
@@ -43,10 +45,15 @@ DISK_FLAVORS=("applehv" "hyperv" "qemu")
 DISK_FLAVORS_W_SUFFIX=("applehv.raw" "hyperv.vhdx" "qemu.qcow2")
 
 REPO="${REPO:-quay.io/podman}"
-DISK_IMAGE_NAME="${DISK_IMAGE_NAME:-stage-machine-os}"
 OUTDIR="${TMT_TEST_DATA:-outdir}"
 BUILD_SCRIPT="./build-podman-machine-os-disks/build-podman-machine-os-disks.sh"
-OCI_NAME="${OCI_NAME:-podman-machine-daily}"
+if [[ ${PODMAN_RPM_TYPE} != "dev" ]]; then
+    OCI_NAME="machine-os-$PODMAN_VERSION"
+    DISK_IMAGE_NAME="$OCI_NAME-$PODMAN_RPM_RELEASE"
+else
+    OCI_NAME="${OCI_NAME:-podman-machine-daily}"
+    DISK_IMAGE_NAME="${DISK_IMAGE_NAME:-stage-machine-os}"
+fi
 OCI_VERSION="${OCI_VERSION:-unknown}"
 FULL_IMAGE_NAME="${REPO}/${OCI_NAME}:${OCI_VERSION}"
 
