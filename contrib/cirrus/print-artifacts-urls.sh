266214dcda277afa0fca02e742f5a4011e66e1ca
Paul Holzinger <pholzing@redhat.com>
2024-11-13 18:47:37 +0100

    add pr comment with artifact download URLs
    So user have easy access to the new URLs without having to click through
    the cirrus web interface. As the action triggers on check_suite this
    will only work once merged into main. We cannot test this on this PR so
    I am not sure if it will work the first time around.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

---
+++ contrib/cirrus/print-artifacts-urls.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CIRRUS_BUILD_ID=$1
+
+if [[ -z "$CIRRUS_BUILD_ID" ]]; then
+    echo "Requires cirrus build as first argument" 1>&2
+    exit 1
+fi
+
+echo "Image Downloads for id ${CIRRUS_BUILD_ID}:"
+echo
+echo "| Name | URL |"
+echo "| --- | --- |"
+
+for arch in x86_64 aarch64; do
+    for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst tar; do
+        name="podman-machine.$arch.$end"
+        echo "| $name | https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name |"
+    done
+done
+
+echo "| Everything zip | https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image.zip |"
