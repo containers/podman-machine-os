de64ed38f5f8cc5a4b8435abb148189d2265b8f3
Paul Holzinger <pholzing@redhat.com>
2024-11-13 14:23:09 +0100

    bump to v5.3.0
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- podman-rpm-info-vars.sh
+++ podman-rpm-info-vars.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Set PODMAN_RPM_TYPE to anything other than "dev" to build release rpms.
-export PODMAN_RPM_TYPE="dev"
+export PODMAN_RPM_TYPE="release"
 
 # If PODMAN_RPM_TYPE is "dev", the vars below don't end up getting used in the
 # build
-export PODMAN_VERSION="5.3.0~rc2"
+export PODMAN_VERSION="5.3.0"
 export PODMAN_RPM_RELEASE="1"
