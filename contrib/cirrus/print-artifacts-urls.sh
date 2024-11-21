a0fd6a1c3b22485c343a4fde0820368a5c6882fe
Paul Holzinger <pholzing@redhat.com>
2024-11-21 15:03:09 +0100

    print-artifacts-urls.sh: update output format
    Create one table with x86_64 and aarch64 and create hyperlinks as there
    is no need to dump the full URL in a github comment. Users can just
    click on the name which makes the output much more human readable.
    
    Also include the link to the cirrus build for easy access the the CI
    status.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- contrib/cirrus/print-artifacts-urls.sh
+++ contrib/cirrus/print-artifacts-urls.sh
@@ -7,16 +7,18 @@ if [[ -z "$CIRRUS_BUILD_ID" ]]; then
     exit 1
 fi
 
-echo "Image Downloads for id ${CIRRUS_BUILD_ID}:"
+echo "Image Downloads for cirrus build [${CIRRUS_BUILD_ID}](https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}):"
 echo
-echo "| Name | URL |"
+echo "| x86_64 | aarch64 |"
 echo "| --- | --- |"
 
-for arch in x86_64 aarch64; do
-    for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst tar; do
+for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst tar; do
+    for arch in x86_64 aarch64; do
         name="podman-machine.$arch.$end"
-        echo "| $name | https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name |"
+        echo -n "| [$name](https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name) "
     done
+    echo "|"
 done
 
-echo "| Everything zip | https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image.zip |"
+echo
+echo "[Everything zip](https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image.zip)"
