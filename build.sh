26362f811b89ad79642989fd5e1de3ef5c96a7c6
Paul Holzinger <pholzing@redhat.com>
2024-11-22 11:38:28 +0100

    use github.com/coreos/custom-coreos-disk-images
    Use the new build script to generate the images.
    Given the new script creates a zip and gz compressed files we must
    uncompress them first in order to compress them again with zstd. This
    makes the script much more complicated than it should be but at least it
    works for now. Given that the script is also slower so we have to bump
    the cirrus timeout.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ source ./util.sh
 
 echo "Preparing to build ${FULL_IMAGE_NAME}"
 
-if [[ ! -d "build-podman-machine-os-disks" ]]; then
-    git clone https://github.com/dustymabe/build-podman-machine-os-disks
-    sed -i -e 's|fedora:quay.io/containers.*"|fedora:quay.io/podman/machine-os:${PODMAN_VERSION%.*}"|' build-podman-machine-os-disks/build-podman-machine-os-disks.sh
+if [[ ! -d "custom-coreos-disk-images" ]]; then
+    # FIXME: pin this to a commit to net get broken all of the sudden
+    git clone https://github.com/coreos/custom-coreos-disk-images
 fi
 
 echo " Building image locally"
@@ -36,12 +36,42 @@ mkdir -p $OUTDIR
 podman save --format oci-archive -o "${OUTDIR}/${DISK_IMAGE_NAME}" "${FULL_IMAGE_NAME_ARCH}"
 
 echo "Transforming OCI image into disk image"
-pushd $OUTDIR && sh $SRCDIR/build-podman-machine-os-disks/build-podman-machine-os-disks.sh "${PWD}/${DISK_IMAGE_NAME}"
+pushd $OUTDIR && sh $SRCDIR/custom-coreos-disk-images/custom-coreos-disk-images.sh \
+  --platforms applehv,hyperv,qemu \
+  --ociarchive "${PWD}/${DISK_IMAGE_NAME}" \
+  --osname fedora-coreos \
+  --imgref "ostree-remote-registry:fedora:quay.io/podman/machine-os:${PODMAN_VERSION%.*}" \
+  --metal-image-size 6144
+
+
+declare -A COREOS_PLATFORM_SUFFIX=(
+    ['applehv']='raw.gz'
+    ['hyperv']='vhdx.zip'
+    ['qemu']='qcow2'
+)
 
 echo "Compressing disk images with zstd"
 # note: we are still "in" the outdir at this point
-for DISK in "${DISK_FLAVORS_W_SUFFIX[@]}"; do
-  zstd --rm -T0 -14 "${DISK_IMAGE_NAME}.${CPU_ARCH}.${DISK}"
+for hypervisor in "${!COREOS_PLATFORM_SUFFIX[@]}"; do
+  extension="${COREOS_PLATFORM_SUFFIX[$hypervisor]}"
+  filename="${DISK_IMAGE_NAME}-${hypervisor}.${CPU_ARCH}.${extension}"
+  case "${extension##*.}" in
+    "gz")
+      time gzip -d $filename
+      # trim of .gz
+      filename="${filename%.*}"
+      ;;
+    "zip")
+      time unzip $filename
+      # trim of .zip
+      filename="${filename%.*}"
+    ;;
+  esac
+
+  newfilename="${DISK_IMAGE_NAME}.${CPU_ARCH}.${hypervisor}.${filename##*.}"
+  mv "$filename" "$newfilename"
+
+  zstd --rm -T0 -14 "$newfilename"
 done
 
 popd
