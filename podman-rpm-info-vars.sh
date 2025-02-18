f719925d57ad86964ddbeffa5802895ef0132a43
Lokesh Mandvekar <lsm5@fedoraproject.org>
2025-02-18 16:08:37 +0530

    Release RPM: Fetch from Podman upstream PR
    This commit will allow fetching the podman rpm from the upstream Podman release
    PR's (or any PR for that matter) Packit copr job.
    
    At this point, the upstream release PR is assumed to already have successful copr rpms.
    
    We do not need a new tag pushed to podman upstream in order for this
    to work.
    
    The copr created by Packit on the Podman release PR will be preserved
    indefinitely.
    
    Note: For now, the PODMAN_VERSION var will also have to be updated
    manually on release PRs.
    
    Co-authored-by: Paul Holzinger <pholzing@redhat.com>
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>
    (cherry picked from commit 090cd3f0fc918f647a408918e9b2d13fc2b6afc9)
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- podman-rpm-info-vars.sh
+++ podman-rpm-info-vars.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Set to "dev" to pull from the podman-next copr, set to "release"
-# to pull the ext rom from the fedora build system based of the versions below.
-export PODMAN_RPM_TYPE="release"
-
-# PODMAN_VERSION is used for fetching the right rpm when PODMAN_RPM_TYPE is set to release.
-# However it is always used to derive the machine-os image tag (x.y) so this must be valid
+# 1. PODMAN_VERSION is always used to derive the machine-os image tag (x.y) so this must be valid
 # at any given time.
-export PODMAN_VERSION="5.4.0"
-export PODMAN_RPM_RELEASE="1"
+# 2. Both PODMAN_VERSION and PODMAN_PR_NUM will have to be updated manually on release
+# PRs.
+# 3. If PODMAN_PR_NUM is empty, rpms will be fetched from the `rhcontainerbot/podman-next` copr.
+export PODMAN_VERSION="5.5.0-dev"
+export PODMAN_PR_NUM=""
