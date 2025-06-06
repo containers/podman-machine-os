dbfab8353803fc6906b48972cf24efc89c93b574
Paul Holzinger <pholzing@redhat.com>
2025-06-06 17:17:17 +0200

    cirrus: use win-sess-launch from podman
    WSL tests cannot be run by the default cirrus context for some reason so
    we must create a extra session.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

---
+++ contrib/cirrus/win-verify.ps1
@@ -0,0 +1,12 @@
+Write-Host "Recovering env. vars."
+Import-CLIXML "$ENV:TEMP\envars.xml" | % {
+    Write-Host "    $($_.Name) = $($_.Value)"
+    Set-Item "Env:$($_.Name)" "$($_.Value)"
+}
+
+$Env:CONTAINERS_MACHINE_PROVIDER = "${ENV:PROVIDER}"
+$Env:MACHINE_IMAGE_PATH="..\${ENV:MACHINE_IMAGE}"
+.\bin\ginkgo -v .\verify
+if ( ($LASTEXITCODE -ne $null) -and ($LASTEXITCODE -ne 0) ) {
+    throw "Exit code = '$LASTEXITCODE' running ginkgo"
+}
