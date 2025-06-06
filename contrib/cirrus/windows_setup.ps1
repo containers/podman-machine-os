e614e00b2e93a96ab057fc58672b34a00e64d06f
Paul Holzinger <pholzing@redhat.com>
2025-06-06 18:14:22 +0200

    update windows installer download link
    The old setup.exe path is deprecated as the name now included the arch
    in it.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- contrib/cirrus/windows_setup.ps1
+++ contrib/cirrus/windows_setup.ps1
@@ -25,7 +25,7 @@ function download($uri, $file) {
 download "${ENV:MACHINE_IMAGE_BASE_URL}${ENV:MACHINE_IMAGE}" "${ENV:MACHINE_IMAGE}"
 
 # Download and install podman
-$uri = "https://github.com/containers/podman/releases/download/v${ENV:PODMAN_INSTALL_VERSION}/podman-${ENV:PODMAN_INSTALL_VERSION}-setup.exe"
+$uri = "https://github.com/containers/podman/releases/download/v${ENV:PODMAN_INSTALL_VERSION}/podman-installer-windows-amd64.exe"
 $installer = "podman-setup.exe"
 download "$uri" "$installer"
 
