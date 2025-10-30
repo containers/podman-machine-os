051203a33c32b2d522e24c1d8d77dcb6ed3663fe
dvorst <87502756+dvorst@users.noreply.github.com>
2025-10-30 13:52:24 +0100

    fix: rootless podman inside rootless podman on WSL
    setup.sh:
    Adjust SUB_UID and SUB_GID ranges to support running rootless Podman inside a rootless run Podman container.
    
    By default, a new user is assigned the following sub-ID ranges:
      SUB_UID_MIN=100000, SUB_GID_MIN=100000, SUB_UID_COUNT=65536, SUB_GID_COUNT=65536
    This means the user’s sub-UID and sub-GID ranges are 100000–165535.
    
    When the container is run rootless with the user defined below, ID mappings occur as follows:
    - Container ID 0 (root) maps to user ID 1000 on the host (which is the user created below).
    - Container IDs 1–65536 map to IDs 100000–165535 on host (the subid range previously mentioned).
    
    If a new user is created inside this container (to build containers for example), it will
    attempt to use the default sub-ID range (100000–165535). However, this exceeds the container’s
    available ID mapping, since only IDs up to 65536 are mapped. This causes nested rootless Podman
    to fail.
    
    To enable container-in-container builds, the sub-ID ranges for the user must be large enough
    to provide at least 65536 usable IDs. A minimum SUB_UID_COUNT and SUB_GID_COUNT of 165536 is
    required, but 200000 is used here to provide additional margin.
    
    Signed-off-by: Dennis Van de Vorst <87502756+dvorst@users.noreply.github.com>

--- contrib/cirrus/setup.sh
+++ contrib/cirrus/setup.sh
@@ -9,8 +9,27 @@ ROOTLESS_UID=$rootless_uid
 rootless_gid=$((1500 + RANDOM % 5000))
 echo "creating $rootless_uid:$rootless_gid $ROOTLESS_USER user"
 groupadd -g $rootless_gid $ROOTLESS_USER
-useradd -g $rootless_gid -u $rootless_uid --no-user-group --create-home $ROOTLESS_USER
 
+# Adjust SUB_UID and SUB_GID ranges to support running rootless Podman inside a rootless run Podman container.
+#
+# By default, a new user is assigned the following sub-ID ranges:
+#   SUB_UID_MIN=100000, SUB_GID_MIN=100000, SUB_UID_COUNT=65536, SUB_GID_COUNT=65536
+# This means the user’s sub-UID and sub-GID ranges are 100000–165535.
+#
+# When the container is run rootless with the user defined below, ID mappings occur as follows:
+# - Container ID 0 (root) maps to user ID 1000 on the host (which is the user created below).
+# - Container IDs 1–65536 map to IDs 100000–165535 on host (the subid range previously mentioned).
+#
+# If a new user is created inside this container (to build containers for example), it will
+# attempt to use the default sub-ID range (100000–165535). However, this exceeds the container’s
+# available ID mapping, since only IDs up to 65536 are mapped. This causes nested rootless Podman
+# to fail.
+#
+# To enable container-in-container builds, the sub-ID ranges for the user must be large enough
+# to provide at least 65536 usable IDs. A minimum SUB_UID_COUNT and SUB_GID_COUNT of 165536 is
+# required, but 200000 is used here to provide additional margin.
+useradd -g $rootless_gid -u $rootless_uid --no-user-group --create-home $ROOTLESS_USER \
+    --key SUB_GID_COUNT=200000 --key SUB_UID_COUNT=200000
 
 # setup ssh access to the user
 mkdir -p "$HOME/.ssh" "/home/$ROOTLESS_USER/.ssh"
