containers/podman-machine-os

commit b0fad2b945c4675898716d62da522760303a62af

Remove Intel Apple builds and artifacts
Podman will no longer support Intel Macs as of Podman 6.  This PR
removes our builds and artifacts for that platform.

JIRA: RUN-3622

Signed-off-by: Brent Baude <bbaude@redhat.com>

.cirrus.yml
@@ -198,9 +198,12 @@ publish_images_task:
         RELEASE_TOKEN: ENCRYPTED[51eebe3e4119c3c51078be7827458df1e63c882851484f95bc0624a0d4ddbed48163debc1afb33711d268d9b444eaa3a]
         LABEL_TOKEN: ENCRYPTED[4095757ab03fa845ea08ab99cb8fe3a560e65de22204fbac07043d076d2f498ab5fedd0491659d8f3baef2c831b482aa]
     push_script: |
+        source ./util.sh
         mkdir $OUTDIR
-        for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst wsl.tar.zst tar; do
-            for arch in x86_64 aarch64; do
+        for arch in x86_64 aarch64; do
+            # Get artifact extensions for this architecture
+            artifact_exts=$(get_artifact_extensions_for_arch "$arch")
+            for end in $artifact_exts; do
                 name="podman-machine.$arch.$end"
                 curl --retry 5 --retry-delay 8 --fail --location -O --output-dir $OUTDIR --url "${MACHINE_IMAGE_BASE_URL}$name"
             done

build.sh
@@ -90,8 +90,11 @@ rpm-ostree compose build-chunked-oci \
         --output "oci-archive:${OUTDIR}/${DISK_IMAGE_NAME}"
 
 echo "Transforming OCI image into disk image"
+# Get supported CoreOS platforms for this architecture and convert space-separated to comma-separated
+# (WSL is built separately and not included here)
+PLATFORMS="${ARCH_TO_COREOS_PLATFORMS[$CPU_ARCH]// /,}"
 pushd "$OUTDIR" && sh "$SRCDIR"/custom-coreos-disk-images/custom-coreos-disk-images.sh \
-  --platforms applehv,hyperv,qemu \
+  --platforms "$PLATFORMS" \
   --ociarchive "${PWD}/${DISK_IMAGE_NAME}" \
   --osname fedora-coreos \
   --imgref "ostree-unverified-registry:$FULL_IMAGE_NAME" \
@@ -107,7 +110,8 @@ declare -A COREOS_PLATFORM_SUFFIX=(
 
 echo "Compressing disk images with zstd"
 # note: we are still "in" the outdir at this point
-for hypervisor in "${!COREOS_PLATFORM_SUFFIX[@]}"; do
+# Only process CoreOS platforms supported by this architecture (WSL is already compressed)
+for hypervisor in ${ARCH_TO_COREOS_PLATFORMS[$CPU_ARCH]}; do
   # Rename the file to our preferred format
   extension="${COREOS_PLATFORM_SUFFIX[$hypervisor]}"
   filename="${DISK_IMAGE_NAME}-${hypervisor}.${CPU_ARCH}.${extension}"

contrib/cirrus/print-artifacts-urls.sh
@@ -7,6 +7,18 @@ if [[ -z "$CIRRUS_BUILD_ID" ]]; then
     exit 1
 fi
 
+# Source util.sh to get platform mappings
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/../../util.sh"
+
+# Helper function to check if an arch supports a platform artifact
+arch_supports_platform() {
+    local arch=$1
+    local platform_ext=$2
+    local supported_exts=$(get_artifact_extensions_for_arch "$arch")
+    [[ " $supported_exts " =~ " $platform_ext " ]]
+}
+
 echo "Image Downloads for cirrus build [${CIRRUS_BUILD_ID}](https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}):"
 echo
 echo "| x86_64 | aarch64 |"
@@ -14,8 +26,12 @@ echo "| --- | --- |"
 
 for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst wsl.tar.zst tar; do
     for arch in x86_64 aarch64; do
-        name="podman-machine.$arch.$end"
-        echo -n "| [$name](https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name) "
+        if arch_supports_platform "$arch" "$end"; then
+            name="podman-machine.$arch.$end"
+            echo -n "| [$name](https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name) "
+        else
+            echo -n "| N/A "
+        fi
     done
     echo "|"
 done

util.sh
@@ -8,6 +8,20 @@ declare -A ARCH_TO_IMAGE_ARCH=(
     ["aarch64"]="arm64"
 )
 
+# Map architectures to their supported disk image platforms
+# Note: WSL is built separately (not via custom-coreos-disk-images)
+# applehv is only supported on Apple Silicon Macs (aarch64)
+declare -A ARCH_TO_PLATFORMS=(
+    ["x86_64"]="hyperv qemu wsl"
+    ["aarch64"]="applehv hyperv qemu wsl"
+)
+
+# Platforms that are built via custom-coreos-disk-images (excludes WSL)
+declare -A ARCH_TO_COREOS_PLATFORMS=(
+    ["x86_64"]="hyperv qemu"
+    ["aarch64"]="applehv hyperv qemu"
+)
+
 
 disk_format_from_flavor () {
   if [[ -z $1 ]]; then
@@ -33,6 +47,39 @@ disk_format_from_flavor () {
       exit 1
       ;;
   esac
+}
+
+# Convert platform name to compressed artifact extension (e.g., "applehv" -> "applehv.raw.zst")
+platform_to_artifact_ext () {
+  local platform=$1
+  if [[ -z $platform ]]; then
+    echo "no platform passed"
+    return 1
+  fi
+
+  local format=$(disk_format_from_flavor "$platform")
+  echo "${platform}.${format}.zst"
+}
+
+# Get artifact extensions for a given architecture (space-separated)
+# Also includes the uncompressed "tar" artifact
+get_artifact_extensions_for_arch () {
+  local arch=$1
+  if [[ -z $arch ]]; then
+    echo "no architecture passed" >&2
+    return 1
+  fi
+
+  local platforms="${ARCH_TO_PLATFORMS[$arch]}"
+  local result=""
+
+  for platform in $platforms; do
+    result="$result $(platform_to_artifact_ext "$platform")"
+  done
+
+  result="$result $arch.tar"
+
+  echo "$result" | xargs  # trim whitespace
 }
 
 

commit 6c421902d32b88ee8e6a631f7778d0f6c2674d04

rework artifact name logic
The apple x86 removal broke the push as the gather script was not
updated.

To try to fix this do a even bigger rework do avoid looping everywhere
again based on the arch and add helper function to get all the artifact
names.

Also update the cirrus print script to make it more robust.
With these changes it should be more trival to add more providers or
even arches without much changes.

Fixes: b0fad2b945 ("Remove Intel Apple builds and artifacts")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

.cirrus.yml
@@ -200,13 +200,8 @@ publish_images_task:
     push_script: |
         source ./util.sh
         mkdir $OUTDIR
-        for arch in x86_64 aarch64; do
-            # Get artifact extensions for this architecture
-            artifact_exts=$(get_artifact_extensions_for_arch "$arch")
-            for end in $artifact_exts; do
-                name="podman-machine.$arch.$end"
-                curl --retry 5 --retry-delay 8 --fail --location -O --output-dir $OUTDIR --url "${MACHINE_IMAGE_BASE_URL}$name"
-            done
+        for name in $(get_all_artifact_names); do
+            curl --retry 5 --retry-delay 8 --fail --location -O --output-dir $OUTDIR --url "${MACHINE_IMAGE_BASE_URL}$name"
         done
 
         source ./gather.sh

build.sh
@@ -101,19 +101,12 @@ pushd "$OUTDIR" && sh "$SRCDIR"/custom-coreos-disk-images/custom-coreos-disk-ima
   --metal-image-size 6144 \
   --extra-kargs='ostree.prepare-root.composefs=0'
 
-
-declare -A COREOS_PLATFORM_SUFFIX=(
-    ['applehv']='raw'
-    ['hyperv']='vhdx'
-    ['qemu']='qcow2'
-)
-
 echo "Compressing disk images with zstd"
 # note: we are still "in" the outdir at this point
 # Only process CoreOS platforms supported by this architecture (WSL is already compressed)
 for hypervisor in ${ARCH_TO_COREOS_PLATFORMS[$CPU_ARCH]}; do
   # Rename the file to our preferred format
-  extension="${COREOS_PLATFORM_SUFFIX[$hypervisor]}"
+  extension="${PLATFORM_TO_DISKTYPE[$hypervisor]}"
   filename="${DISK_IMAGE_NAME}-${hypervisor}.${CPU_ARCH}.${extension}"
   newfilename="${DISK_IMAGE_NAME}.${CPU_ARCH}.${hypervisor}.${extension}"
   mv "$filename" "$newfilename"

contrib/cirrus/print-artifacts-urls.sh
@@ -11,29 +11,43 @@ fi
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/../../util.sh"
 
-# Helper function to check if an arch supports a platform artifact
-arch_supports_platform() {
-    local arch=$1
-    local platform_ext=$2
-    local supported_exts=$(get_artifact_extensions_for_arch "$arch")
-    [[ " $supported_exts " =~ " $platform_ext " ]]
-}
-
 echo "Image Downloads for cirrus build [${CIRRUS_BUILD_ID}](https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}):"
 echo
-echo "| x86_64 | aarch64 |"
-echo "| --- | --- |"
 
-for end in applehv.raw.zst hyperv.vhdx.zst qemu.qcow2.zst wsl.tar.zst tar; do
-    for arch in x86_64 aarch64; do
-        if arch_supports_platform "$arch" "$end"; then
-            name="podman-machine.$arch.$end"
+echo -n "| Provider "
+
+line="| --- "
+for arch in "${!ARCH_TO_PLATFORMS[@]}"; do
+  echo -n "| $arch "
+  line+="| --- "
+done
+echo "|"
+line+="|"
+echo "$line"
+
+declare -A map
+declare -A providers
+
+# sort names by hypervisor
+for name in $(get_all_artifact_names); do
+    disk_type="$(get_disk_type_from_name $name)"
+    arch="$(get_arch_from_name $name)"
+    map["$disk_type-$arch"]="$name"
+    providers["$disk_type"]=
+done
+
+for provider in ${!providers[*]} ; do
+    echo -n "| $provider "
+    for arch in "${!ARCH_TO_PLATFORMS[@]}"; do
+        name="${map["$provider-$arch"]}"
+        if [ -n "$name" ]; then
             echo -n "| [$name](https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/image_build/image/$name) "
         else
             echo -n "| N/A "
         fi
     done
-    echo "|"
+
+     echo "|"
 done
 
 echo

gather.sh
@@ -15,21 +15,20 @@ fi
 buildah manifest create $cirrusBuildIdAnnoation "${FULL_IMAGE_NAME}"
 
 # Load and add OCI image to manifest
-for arch in "${!ARCH_TO_IMAGE_ARCH[@]}"; do
-  output=$(podman load -i "${OUTDIR}/${DISK_IMAGE_NAME}.${arch}.tar")
+for name in $(get_oci_artifact_names); do
+  output=$(podman load -i "${OUTDIR}/$name")
+  arch="${ARCH_TO_IMAGE_ARCH[$(get_arch_from_name $name)]}"
   # Trim "Loaded image: " prefix
   id="${output#Loaded image: }"
-  image_name="$FULL_IMAGE_NAME-${ARCH_TO_IMAGE_ARCH[$arch]}"
+  image_name="$FULL_IMAGE_NAME-$arch"
   podman tag "$id" "$image_name"
-  buildah manifest add --arch ${ARCH_TO_IMAGE_ARCH[$arch]} "${FULL_IMAGE_NAME}" "$image_name"
+  buildah manifest add --arch $arch "${FULL_IMAGE_NAME}" "$image_name"
 done
 
 
 # Adds the OCI artifacts to the manifest
-for itype in "${DISK_FLAVORS[@]}"; do
-  for arch in "${!ARCH_TO_IMAGE_ARCH[@]}"; do
-    DISK_FORMAT=$(disk_format_from_flavor "${itype}")
-    COMPRESSED_DISK="${OUTDIR}/${DISK_IMAGE_NAME}.${arch}.${itype}.${DISK_FORMAT}.zst"
-	  buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${itype}" "${FULL_IMAGE_NAME}" "$COMPRESSED_DISK"
-  done
+for name in $(get_disk_artifact_names); do
+    disk_type="$(get_disk_type_from_name $name)"
+    arch="$(get_arch_from_name $name)"
+    buildah manifest add --artifact --artifact-type=""  --os=linux --arch=$arch --annotation "disktype=${disk_type}" "${FULL_IMAGE_NAME}" "${OUTDIR}/$name"
 done

util.sh
@@ -3,6 +3,8 @@
 source ./podman-rpm-info-vars.sh
 
 CPU_ARCH=$(uname -m)
+export DISK_IMAGE_NAME="podman-machine"
+
 declare -A ARCH_TO_IMAGE_ARCH=(
     ["x86_64"]="amd64"
     ["aarch64"]="arm64"
@@ -22,74 +24,50 @@ declare -A ARCH_TO_COREOS_PLATFORMS=(
     ["aarch64"]="applehv hyperv qemu"
 )
 
+declare -A PLATFORM_TO_DISKTYPE=(
+    ["applehv"]="raw"
+    ["hyperv"]="vhdx"
+    ["qemu"]="qcow2"
+    ["wsl"]="tar"
+)
 
-disk_format_from_flavor () {
-  if [[ -z $1 ]]; then
-    echo "no disk flavor passed"
-    exit
-  fi
-
-  case $1 in
-    "applehv")
-      echo "raw"
-      ;;
-    "hyperv")
-      echo "vhdx"
-      ;;
-    "qemu")
-      echo "qcow2"
-      ;;
-    "wsl")
-      echo "tar"
-      ;;
-    *)
-      echo "unknown flavor $1"
-      exit 1
-      ;;
-  esac
-}
-
-# Convert platform name to compressed artifact extension (e.g., "applehv" -> "applehv.raw.zst")
-platform_to_artifact_ext () {
-  local platform=$1
-  if [[ -z $platform ]]; then
-    echo "no platform passed"
-    return 1
-  fi
-
-  local format=$(disk_format_from_flavor "$platform")
-  echo "${platform}.${format}.zst"
-}
-
-# Get artifact extensions for a given architecture (space-separated)
-# Also includes the uncompressed "tar" artifact
-get_artifact_extensions_for_arch () {
-  local arch=$1
-  if [[ -z $arch ]]; then
-    echo "no architecture passed" >&2
-    return 1
-  fi
-
-  local platforms="${ARCH_TO_PLATFORMS[$arch]}"
-  local result=""
-
-  for platform in $platforms; do
-    result="$result $(platform_to_artifact_ext "$platform")"
+get_disk_artifact_names() {
+  for arch in "${!ARCH_TO_PLATFORMS[@]}"; do
+    for platform in ${ARCH_TO_PLATFORMS[$arch]}; do
+      echo "$DISK_IMAGE_NAME.$arch.$platform.${PLATFORM_TO_DISKTYPE[$platform]}.zst"
+    done
   done
-
-  result="$result $arch.tar"
-
-  echo "$result" | xargs  # trim whitespace
 }
 
+get_oci_artifact_names() {
+  for arch in "${!ARCH_TO_PLATFORMS[@]}"; do
+    echo "$DISK_IMAGE_NAME.$arch.tar"
+  done
+}
 
-DISK_FLAVORS=("applehv" "hyperv" "qemu" "wsl")
+get_all_artifact_names() {
+  get_disk_artifact_names
+  get_oci_artifact_names
+}
+
+_get_part_from_name() {
+  local array
+  IFS='.' read -r -a array <<< "$1"
+  echo "${array[$2]}"
+}
+
+get_arch_from_name() {
+  _get_part_from_name "$1" 1
+}
+
+get_disk_type_from_name() {
+  _get_part_from_name "$1" 2
+}
 
 # OUTDIR needs to be run in TMT test as a non-root user after initial root
 # login
 export SRCDIR="${TMT_TREE:-${CIRRUS_WORKING_DIR:-..}}"
 export OUTDIR="${OUTDIR:-${TMT_TEST_DATA:-$(git rev-parse --show-toplevel)/outdir}}"
-export DISK_IMAGE_NAME="podman-machine"
 
 REPO="${REPO:-quay.io/podman}"
 OCI_NAME="machine-os"